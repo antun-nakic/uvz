db0ff2673185618e8c31f74ac46b38603ce5063e
Antun Nakić <antun.nakic@digital-abundance.hr>
2022-07-14 21:22:17 +0200

    inicijalni commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Vijest from "./Vijest";
 
 function App() {
+  let sime = "Dobar dan svijete!";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div className='App'>
+      <Vijest />
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <p>{sime} jfhgjfdkhgjkfd</p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'>
           Learn React
         </a>
       </header>
